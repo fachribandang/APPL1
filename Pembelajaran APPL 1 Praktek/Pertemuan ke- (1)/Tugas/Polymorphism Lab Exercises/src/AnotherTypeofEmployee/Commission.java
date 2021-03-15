1fb31500faee384a78bb937361f50e1ad36571a8
Fachri Muhammad Bandang Haq <60908604+fachribandang@users.noreply.github.com>
2021-03-15 12:17:28 +0700

    soal no 1

--- Pembelajaran APPL 1 Praktek/Pertemuan ke- (1)/Tugas/Polymorphism Lab Exercises/src/AnotherTypeofEmployee/Commission.java
+++ Pembelajaran APPL 1 Praktek/Pertemuan ke- (1)/Tugas/Polymorphism Lab Exercises/src/AnotherTypeofEmployee/Commission.java
@@ -1,4 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package AnotherTypeofEmployee;
+
+
 //******************************************************************** 
 // Firm.java Author: Lewis/Loftus 
 // 
@@ -10,8 +17,7 @@ NIM 		:191524042
 Course		:D4-1B
 E-mail 		:fachri.muhammad.tif419@polban.ac.id
 Date		:２０２１年３月１５日
-
  */
-public class main {
+public class Commission {
     
 }
