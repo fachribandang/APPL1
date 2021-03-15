ad424c7e430aff3da331639a526d831135472fdf
Fachri Muhammad Bandang Haq <60908604+fachribandang@users.noreply.github.com>
2021-03-15 10:55:32 +0700

    Update main.java

--- Pembelajaran APPL 1 Praktek/Pertemuan ke- (1)/Tugas/Polymorphism Lab Exercises/src/AnotherTypeofEmployee/main.java
+++ Pembelajaran APPL 1 Praktek/Pertemuan ke- (1)/Tugas/Polymorphism Lab Exercises/src/AnotherTypeofEmployee/main.java
@@ -1,14 +1,12 @@
 package AnotherTypeofEmployee;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Created By 	:Fachri Muhammad Bandang Haq
+NIM 		:191524042
+Course		:D4-1B
+E-mail 		:fachri.muhammad.tif419@polban.ac.id
+Date		:２０２１年３月１５日
 
-/**
- *
- * @author fachr
  */
 public class main {
     
