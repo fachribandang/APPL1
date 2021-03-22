3acf2c671d05754d0d5b644f5a35a78277d5fd24
Fachri Muhammad Bandang Haq <60908604+fachribandang@users.noreply.github.com>
2021-03-22 13:55:18 +0700

    up

---
+++ Pembelajaran APPL 1 Praktek/Pertemuan ke- (2)/Tugas/JavaApplication2/src/ReadingfromandWritingtoTextFiles/Warning.java
@@ -0,0 +1,63 @@
+
+package ReadingfromandWritingtoTextFiles;
+// **************************************************************************** 
+// Warning.java 
+// 
+// Reads student data from a text file and writes data to another text file. 
+// **************************************************************************** 
+import java.util.Scanner; 
+import java.io.*; 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;
+public class Warning 
+{ 
+ // -------------------------------------------------------------------- 
+ // Reads student data (name, semester hours, quality points) from a 
+ // text file, computes the GPA, then writes data to another file 
+ // if the student is placed on academic warning. 
+ // -------------------------------------------------------------------- 
+ public static void main (String[] args) throws FileNotFoundException 
+ { 
+ int creditHrs; // number of semester hours earned 
+ double qualityPts; // number of quality points earned 
+ double gpa; // grade point (quality point) average 
+ String line, name, inputName = "students.dat"; 
+ String outputName = "warning.dat"; 
+try 
+ { 
+ // Set up scanner to input file 
+ // Set up the output file stream 
+ // Print a header to the output file 
+ File  FileObject= new File(inputName); 
+ Scanner FileReader = new Scanner(FileObject);
+ FileWriter FileWriter;
+ outFile.println (); 
+ outFile.println ("Students on Academic Warning"); 
+ outFile.println (); 
+ // Process the input file, one token at a time 
+ 
+ while (true) 
+ { 
+ // Get the credit hours and quality points and 
+ // determine if the student is on warning. If so, 
+ // write the student data to the output file. 
+ } 
+ // Close output file 
+ } 
+// catch (FileNotFoundException exception) 
+// { 
+// System.out.println ("The file " + inputName + " was not found."); 
+// } 
+// catch (IOException exception) 
+// { 
+// System.out.println (exception); 
+// } 
+ catch (NumberFormatException e) 
+ { 
+ System.out.println ("Format error in input file: " + e); 
+ } 
+ } 
+}
---
+++ Pembelajaran APPL 1 Praktek/Pertemuan ke- (2)/Tugas/JavaApplication2/src/ReadingfromandWritingtoTextFiles/outFile.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ReadingfromandWritingtoTextFiles;
+
+/**
+ *
+ * @author fachr
+ */
+class outFile {
+
+    static void println() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void println(String students_on_Academic_Warning) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
