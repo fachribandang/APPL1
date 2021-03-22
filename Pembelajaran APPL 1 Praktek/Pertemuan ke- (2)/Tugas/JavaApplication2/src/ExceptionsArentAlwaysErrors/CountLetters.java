017df5ea2943a118b3da58396835cd473f43f7cd
Fachri Muhammad Bandang Haq <60908604+fachribandang@users.noreply.github.com>
2021-03-22 10:50:59 +0700

    Update CountLetters.java
    menambahkan try catch dalam bagian looping membaca character

--- Pembelajaran APPL 1 Praktek/Pertemuan ke- (2)/Tugas/JavaApplication2/src/ExceptionsArentAlwaysErrors/CountLetters.java
+++ Pembelajaran APPL 1 Praktek/Pertemuan ke- (2)/Tugas/JavaApplication2/src/ExceptionsArentAlwaysErrors/CountLetters.java
@@ -1,16 +1,38 @@
+// **************************************************************** 
+// CountLetters.java 
+// 
+// Reads a words from the standard input and prints the number of 
+// occurrences of each letter in that word. 
+// 
+// ****************************************************************
 package ExceptionsArentAlwaysErrors;
 import java.util.Scanner; 
 public class CountLetters 
 { 
  public static void main(String[] args) 
  { 
- int[] counts = new int[26]; 
- Scanner scan = new Scanner(System.in); 
+ int[] counts = new int[26];    
+ Scanner scan = new Scanner(System.in);
+
+ //get word from user  
  System.out.print("Enter a single word (letters only, please): "); 
- String word = scan.nextLine(); 
+ String word = scan.nextLine();
+
+ //convert to all upper case  
  word = word.toUpperCase(); 
- for (int i=0; i < word.length(); i++) 
+ 
+ //convert to all upper case 
+ try {
+  //  Block of code to try
+   for (int i=0; i < word.length(); i++) 
     counts[word.charAt(i)-'A']++; 
+}
+catch(Exception e) {
+      System.out.println("Not a Letter!");
+      System.out.println("ada yang bukan termasuk huruf");
+}
+ 
+ //print frequencies 
  System.out.println(); 
  for (int i=0; i < counts.length; i++) 
     if (counts [i] != 0) 
